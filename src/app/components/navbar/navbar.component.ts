7a8e5ea057e4fb6cc7b2e60c30011ad24cb9bd68
Angell Pimienta's <angelefrain1510@gmail.com>
2025-02-12 07:32:27 -0500

    feat(component)

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
 
+  lista = [
+    "Productos",
+    "Categorias",
+    "Proveedores",
+    "Ventas",
+    "Reportes",
+    "Usuarios",
+    "Configuración"
+
+  ]
 }
