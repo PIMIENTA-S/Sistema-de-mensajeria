38af59bef502f8c5594caf73396e89290a932135
Angell Pimienta's <angelefrain1510@gmail.com>
2025-02-13 11:00:56 -0500

    feat: refactoring nav | sidebar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,23 +1,42 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { Dash } from '../../interface/dash';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterOutlet, RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
 
-  lista = [
-    "Productos",
-    "Categorias",
-    "Proveedores",
-    "Ventas",
-    "Reportes",
-    "Usuarios",
-    "Configuración"
-
+  lista: Dash[] = [
+    {
+      name:"Productos",
+      route: "/productos",
+      icon: 'fas fa-home'
+    },
+    {
+      name:"Categorias",
+      route: "/categorias",
+      icon: 'fas fa-home'
+    },
+    {
+      name:"Proveedores",
+      route: "/proveedores",
+      icon: 'fas fa-home'
+    },
+    {
+      name:"Reportes",
+      route: "/reportes",
+      icon: 'fas fa-home'
+    },
+    {
+      name:"Configuración",
+      route: "/configuracion",
+      icon: 'fas fa-home'
+    }
   ]
 }
