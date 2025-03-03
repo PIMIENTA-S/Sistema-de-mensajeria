7d03414863af73f48f9190a3782836a7e5f690bc
Angell Pimienta's <angelefrain1510@gmail.com>
2025-03-03 00:42:14 -0500

    tester

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { ProductListComponent } from './pages/product-list/product-list.component';
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from "./pages/login/login.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavbarComponent, RouterOutlet],
+  imports: [NavbarComponent, RouterOutlet, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
