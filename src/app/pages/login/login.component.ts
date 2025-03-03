cea8eec232666fb37935d03bf39d4a98f66255eb
Angell Pimienta's <angelefrain1510@gmail.com>
2025-03-03 00:41:47 -0500

    feat: page for login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,44 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginForm!: FormGroup;
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  // Getter para acceso fácil a los campos del formulario
+  get f() { 
+    return this.loginForm.controls; 
+  }
+
+  onSubmit(): void {
+    this.submitted = true;
+
+    // Detener si el formulario es inválido
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    // Implementar lógica de autenticación aquí
+    console.log('Credenciales del formulario:', this.loginForm.value);
+    
+
+  }
 
 }
